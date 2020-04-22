b195637309019d385d5753d2c08fe51518016dd8
杨大 <yjp138557@antfin.com>
2020-04-22 21:01:09 +0800

    init

---
+++ src/main/java/cc/wsyw126/MySQLG4/MySqlg4Application.java
@@ -0,0 +1,13 @@
+package cc.wsyw126.MySQLG4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlg4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySqlg4Application.class, args);
+	}
+
+}
---
+++ src/test/java/cc/wsyw126/MySQLG4/MySqlg4ApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.wsyw126.MySQLG4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySqlg4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
